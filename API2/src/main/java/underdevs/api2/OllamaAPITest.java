3b8658ce6b4bf028f01a81119ed6cc0731a9379d
Wesley Xavier <wesley.xvier@gmail.com>
2025-03-19 20:11:59 -0300

    :sparkles: feat: adicionado código de abertura de arquivo alvo, adicionado prompt de teste simples, função para gravar o resultado em arquivo.

--- API2/src/main/java/underdevs/api2/OllamaAPITest.java
+++ API2/src/main/java/underdevs/api2/OllamaAPITest.java
@@ -11,6 +11,11 @@ import io.github.ollama4j.utils.OptionsBuilder;
 
 // import java.util.List;
 
+// Parte responsável pela abertura de arquivos.
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class OllamaAPITest {
     public static void main(String[] args) {
         OllamaAPI ollamaAPI = new OllamaAPI(); // Cria o objeto principal
@@ -19,12 +24,17 @@ public class OllamaAPITest {
         // System.out.println("Is Ollama server running: " + isOllamaServerReachable);
         
         try{
+            String content = new String(Files.readAllBytes(Paths.get("test.py")));
             // List<Model> models = ollamaAPI.listModels();
             // models.forEach(model -> System.out.println(model.getName()));
             OllamaResult result;
             ollamaAPI.setRequestTimeoutSeconds(120); // setando tempo de resposta
-            result = ollamaAPI.generate("qwen2.5-coder", "diga: olá", false, new OptionsBuilder().build()); // envia uma pergunta simples
+            result = ollamaAPI.generate("qwen2.5-coder", "create a unit test for the script showing only the text code without any additional text without markdown: " + content, false, new OptionsBuilder().build()); // envia uma pergunta simples
             System.out.println("Result: " + result.getResponse());
+            Files.write(Paths.get("output.py"), result.getResponse().getBytes());
+        }
+        catch (IOException e){
+            System.err.println("Erro ao abrir o arquivo: " + e.getMessage());
         }
         catch (Exception e){
             System.out.println(e.getMessage());
