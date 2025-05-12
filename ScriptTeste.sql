8e9c779dfa157cbc25e53d4b25a25642862e8510
Davi Helisson <davihelisson@gmail.com>
2025-05-12 10:40:14 -0300

    :books: docs: Upload de arquivo Script para SQL

---
+++ ScriptTeste.sql
@@ -0,0 +1,10 @@
+CREATE TABLE melhorias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    codigo_original TEXT NOT NULL,
+    melhoria_sugerida TEXT NOT NULL,
+    data_sugestao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    utilizada BOOLEAN DEFAULT FALSE
+);
+
+SELECT id, codigo_original, melhoria_sugerida, data_sugestao FROM sugestoes 
+WHERE utilizada = false ORDER BY data_sugestao DESC;
